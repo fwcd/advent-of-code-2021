e9a89af827cfb0b0e5433c49e0ff8407abc33c06
fwcd <fwcdmail@gmail.com>
2021-12-01 08:12:47 +0100

    Implement day 1 part 1 in Obj-C

--- day01/src/day01.m
+++ day01/src/day01.m
@@ -1,6 +1,29 @@
 #import <Foundation/Foundation.h>
 
+NSArray *readInput() {
+  NSString *input = [NSString stringWithContentsOfFile:@"resources/input.txt" encoding:NSUTF8StringEncoding error:nil];
+  NSArray *lines = [input componentsSeparatedByString:@"\n"];
+  return lines;
+}
+
+int computeIncreaseSteps(NSArray *input) {
+  int last = [input[0] intValue];
+  int steps = 0;
+  for (int i = 1; i < [input count]; i++) {
+    int current = [input[i] intValue];
+    if (current > last) {
+      steps++;
+    }
+    last = current;
+  }
+  return steps;
+}
+
 int main(void) {
-  NSLog(@"Hello world");
-  return 0;
+  @autoreleasepool {
+    NSArray *input = readInput();
+    int part1 = computeIncreaseSteps(input);
+    NSLog(@"Part 1: %d", part1);
+    return 0;
+  }
 }
