4c3b445f15b722890fed57c4cffecf1ed6a2fac1
fwcd <fwcdmail@gmail.com>
2021-12-01 08:22:09 +0100

    Complete day 1

--- day01/src/day01.m
+++ day01/src/day01.m
@@ -6,15 +6,18 @@ NSArray *readInput() {
   return lines;
 }
 
-int computeIncreaseSteps(NSArray *input) {
-  int last = [input[0] intValue];
+int computeIncreaseSteps(NSArray *input, int windowSize) {
+  int window = [input[0] intValue];
   int steps = 0;
   for (int i = 1; i < [input count]; i++) {
-    int current = [input[i] intValue];
-    if (current > last) {
-      steps++;
+    int last = window;
+    window += [input[i] intValue];
+    if (i >= windowSize) {
+      window -= [input[i - windowSize] intValue];
+      if (window > last) {
+        steps++;
+      }
     }
-    last = current;
   }
   return steps;
 }
@@ -22,8 +25,10 @@ int computeIncreaseSteps(NSArray *input) {
 int main(void) {
   @autoreleasepool {
     NSArray *input = readInput();
-    int part1 = computeIncreaseSteps(input);
+    int part1 = computeIncreaseSteps(input, 1);
     NSLog(@"Part 1: %d", part1);
+    int part2 = computeIncreaseSteps(input, 3);
+    NSLog(@"Part 2: %d", part2);
     return 0;
   }
 }
