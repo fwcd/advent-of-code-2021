d58f503e3a1a3477dc986c1291c1a20e3f197b84
fwcd <fwcdmail@gmail.com>
2021-12-03 06:21:29 +0100

    Use bit math to compute part 1 of day 3

--- day03/src/day03.py
+++ day03/src/day03.py
@@ -1,11 +1,14 @@
-import sys
+import functools
 
 with open('resources/input.txt', 'r') as f:
     lines = [l.strip() for l in f.readlines()]
     width = len(lines[0])
-    counts = [sum(int(word[i]) for word in lines) for i in range(width)]
     threshold = len(lines) / 2
-    raw_gamma_rate = [('1' if c > threshold else '0') for c in counts]
-    gamma_rate = int(''.join(raw_gamma_rate), 2)
+
+    def most_common_bit(i):
+        count = sum(int(word[i]) for word in lines)
+        return 1 if count > threshold else 0
+
+    gamma_rate = functools.reduce(lambda x, y: x | y, [most_common_bit(i) << (width - 1 - i) for i in range(width)])
     epsilon_rate = gamma_rate ^ ((1 << width) - 1) # python has no unary bitwise negation operator, therefore this
     print(f'Part 1: {gamma_rate * epsilon_rate}')
