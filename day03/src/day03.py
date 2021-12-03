3a4538ca7b071319ee48cdb998f5ceeea138f68c
fwcd <fwcdmail@gmail.com>
2021-12-03 06:23:04 +0100

    Use even more funky bit math for day 3 part 1

--- day03/src/day03.py
+++ day03/src/day03.py
@@ -2,13 +2,14 @@ import functools
 
 with open('resources/input.txt', 'r') as f:
     lines = [l.strip() for l in f.readlines()]
+    xs = [int(l, 2) for l in lines]
     width = len(lines[0])
-    threshold = len(lines) / 2
+    threshold = len(xs) / 2
 
     def most_common_bit(i):
-        count = sum(int(word[i]) for word in lines)
+        count = sum((word >> i) & 1 for word in xs)
         return 1 if count > threshold else 0
 
-    gamma_rate = functools.reduce(lambda x, y: x | y, [most_common_bit(i) << (width - 1 - i) for i in range(width)])
+    gamma_rate = functools.reduce(lambda x, y: x | y, [most_common_bit(i) << i for i in range(width)])
     epsilon_rate = gamma_rate ^ ((1 << width) - 1) # python has no unary bitwise negation operator, therefore this
     print(f'Part 1: {gamma_rate * epsilon_rate}')
