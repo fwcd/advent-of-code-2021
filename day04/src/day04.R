bdc161ede5751ad8b7f5dca379735d66fc29b256
fwcd <fwcdmail@gmail.com>
2021-12-04 13:07:43 +0100

    Read day 4 input into board matrices

--- day04/src/day04.R
+++ day04/src/day04.R
@@ -1,9 +1,11 @@
 library(readr)
+library(stringr)
 
-input <- read_file("resources/input.txt")
-components <- strsplit(input, "\n\n")[[1]]
-draws <- strsplit(components[1], ",")[[1]]
-boards <- strsplit(components[2:length(components)], "\n")
+input <- str_trim(read_file("resources/input.txt"))
+components <- str_split(input, "\n\n")[[1]]
+draws <- str_split(components[1], ",")[[1]]
+raw_boards <- str_split(components[2:length(components)], "\n")
+boards <- lapply(raw_boards, function(b) { do.call(rbind, str_split(str_trim(b), "\\s+")) })
 
 print("Draws")
 print(draws)
