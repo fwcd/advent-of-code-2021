0083f0ffd14d77b6dfc276097e94193dc71de195
fwcd <fwcdmail@gmail.com>
2021-12-04 13:26:25 +0100

    Parse day 4 boards to integers

--- day04/src/day04.R
+++ day04/src/day04.R
@@ -1,13 +1,17 @@
 library(readr)
 library(stringr)
 
+# Load input data
 input <- str_trim(read_file("resources/input.txt"))
 components <- str_split(input, "\n\n")[[1]]
-draws <- str_split(components[1], ",")[[1]]
+draws <- strtoi(str_split(components[1], ",")[[1]])
 raw_boards <- str_split(components[2:length(components)], "\n")
-boards <- lapply(raw_boards, function(b) { do.call(rbind, str_split(str_trim(b), "\\s+")) })
+boards <- lapply(raw_boards, function(b) { do.call(rbind, lapply(str_split(str_trim(b), "\\s+"), strtoi)) })
 
-print("Draws")
-print(draws)
-print("Boards")
-print(boards)
+bingo_sum <- function(board) {
+  
+}
+
+for (i in 1:length(draws)) {
+  prefix <- draws[1:i]
+}
