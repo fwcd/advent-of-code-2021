5b556eb7762361aa04a08f785c81149b780ee9af
fwcd <fwcdmail@gmail.com>
2021-12-05 13:09:38 +0100

    Add function for showing grid

--- day05/app/Main.hs
+++ day05/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Control.Monad.State ( State(..), get, put )
+import Control.Monad.State ( State(..), get, put, execState )
 import Data.Char (isSpace)
 import Data.List (isPrefixOf)
 import qualified Data.Vector as V
@@ -20,6 +20,22 @@ trim :: String -> String
 trim = f . f
   where f = reverse . dropWhile isSpace
 
+count :: (a -> Bool) -> V.Vector a -> Int
+count f v = count' 0 0
+  where count' acc n | n == V.length v = acc
+                     | f (v V.! n)     = count' (acc + 1) (n + 1)
+                     | otherwise       = count' acc (n + 1)
+
+maxSize :: Line -> Int
+maxSize (Line (Point x1 y1) (Point x2 y2)) = max (max x1 y1) (max x2 y2)
+
+pretty :: Grid -> String
+pretty (Grid d w) = unlines $ show' 0
+  where show' n | n <= (V.length d - w) = V.foldr (<>) "" (V.map showValue (V.slice n w d)) : show' (n + w)
+                | otherwise             = []
+        showValue 0 = "."
+        showValue n = show n
+
 parsePoint :: String -> Point
 parsePoint raw = Point x y
   where [x, y] = read . trim <$> splitOn "," raw
@@ -37,7 +53,13 @@ insertLine (Line (Point x1 y1) (Point x2 y2)) = do
                     in (i, (d V.! i) + 1)
   put (Grid (d V.// updates) w)
 
+part1 :: Grid -> Int
+part1 = count (> 1) . gData
+
 main :: IO ()
 main = do
   ls <- (parseLine <$>) . lines <$> readFile "resources/demo.txt"
-  print ls
+  let w = maximum (maxSize <$> ls) + 1
+      g = execState (mapM insertLine ls) (Grid (V.replicate (w * w) 0) w)
+  putStrLn $ pretty g
+  putStrLn $ "Part 1: " ++ show (part1 g)
