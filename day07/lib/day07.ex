69ea4942aa7649963cd615a9968c0c6f0834106c
fwcd <fwcdmail@gmail.com>
2021-12-07 12:02:02 +0100

    Load demo inputs in day 7

--- day07/lib/day07.ex
+++ day07/lib/day07.ex
@@ -1,5 +1,8 @@
 defmodule Day07 do
   def main do
-    :world
+    input = File.read!("resources/demo.txt")
+          |> String.split(",")
+          |> Enum.map(fn raw -> String.to_integer(String.trim(raw)) end)
+    input
   end
 end
