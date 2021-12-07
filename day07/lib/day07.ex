12c88fa8c1cd2b2e90b93832371bbb73fb97b716
fwcd <fwcdmail@gmail.com>
2021-12-07 12:09:21 +0100

    Implement day 7 part 1 in Elixir

--- day07/lib/day07.ex
+++ day07/lib/day07.ex
@@ -1,8 +1,15 @@
 defmodule Day07 do
   def main do
-    input = File.read!("resources/demo.txt")
+    input = File.read!("resources/input.txt")
           |> String.split(",")
           |> Enum.map(fn raw -> String.to_integer(String.trim(raw)) end)
-    input
+
+    part1 = input
+          |> Enum.map(fn pos -> input
+            |> Enum.map(fn x -> abs(x - pos) end)
+            |> Enum.sum
+          end)
+          |> Enum.min
+    IO.puts "Part 1: #{part1}"
   end
 end
