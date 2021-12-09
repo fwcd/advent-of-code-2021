4f0bfda5573fdbe2d76760c03dafad06828b774e
fwcd <fwcdmail@gmail.com>
2021-12-09 13:58:49 +0100

    Move day 9 neighbors into subroutine

--- day09/src/day09.pl
+++ day09/src/day09.pl
@@ -16,26 +16,32 @@ while (<FH>) {
 my $width = scalar @{ $lava_map[0] };
 my $height = @lava_map;
 
+sub neighbors {
+  my $y = $_[0];
+  my $x = $_[1];
+  map {
+    my $dy = $_;
+    map {
+      my $dx = $_;
+      my $nx = $x + $dx;
+      my $ny = $y + $dy;
+      if ((($dx == 0) ^ ($dy == 0)) && $nx >= 0 && $nx < $width && $ny >= 0 && $ny < $height) {
+        $lava_map[$ny][$nx];
+      } else {
+        @{ [] };
+      }
+    } (-1..1);
+  } (-1..1);
+}
+
 my @valleys = map {
   my $y = $_;
   map {
     my $x = $_;
-    my @neighbors = map {
-      my $dy = $_;
-      map {
-        my $dx = $_;
-        my $nx = $x + $dx;
-        my $ny = $y + $dy;
-        if ((($dx == 0) ^ ($dy == 0)) && $nx >= 0 && $nx < $width && $ny >= 0 && $ny < $height) {
-          $lava_map[$ny][$nx];
-        } else {
-          @{ [] };
-        }
-      } (-1..1);
-    } (-1..1);
+    my @ns = neighbors $y, $x;
     my $value = $lava_map[$y][$x];
 
-    if ($value < min @neighbors) {
+    if ($value < min @ns) {
       \@{ [$y, $x] };
     } else {
       @{ [] };
