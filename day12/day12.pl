2427c48ef55b0ec61e81e05c0b4a899d03c72865
fwcd <fwcdmail@gmail.com>
2021-12-12 18:32:54 +0100

    Add graph utils and rename dcg productions

--- day12/day12.pl
+++ day12/day12.pl
@@ -1,18 +1,23 @@
 :- use_module(library(pio)).
 :- use_module(library(dcg/basics)).
 
+% Graph utils
+
+edge(V, W, [e(V, W)|_]).
+edge(V, W, [_|Es]) :- edge(V, W, Es).
+
 % DCG for parsing the input
 
-edges([])     --> eos, !.
-edges([E|Es]) --> edge(E), edges(Es).
+dcg_edges([])     --> eos, !.
+dcg_edges([E|Es]) --> dcg_edge(E), dcg_edges(Es).
 
-edge(e(V, W)) --> string(VC), "-", string(WC), eol, !,
+dcg_edge(e(V, W)) --> string(VC), "-", string(WC), eol, !,
   { atom_codes(V, VC), atom_codes(W, WC) }.
 
 % Main program
 
 parse_input(Es) :-
-  phrase_from_file(edges(Es), 'resources/demo.txt').
+  phrase_from_file(dcg_edges(Es), 'resources/demo.txt').
 
 main :-
   parse_input(_).
