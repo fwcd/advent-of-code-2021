8057a2f4571ce097eb864363b937706415a42300
fwcd <fwcdmail@gmail.com>
2021-12-14 07:38:03 +0100

    Complete day 14 part 2

--- day14/src/day14.py
+++ day14/src/day14.py
@@ -1,36 +1,33 @@
 from collections import Counter
 
-with open('resources/demo.txt', 'r') as f:
+with open('resources/input.txt', 'r') as f:
     lines = [l.strip() for l in f.readlines()]
     polymer = lines[0]
     rules = {lhs: rhs for [lhs, rhs] in (l.split(' -> ') for l in lines[2:])}
 
     def score(elem_counts: dict) -> int:
         values = sorted(elem_counts.values())
-        print(elem_counts)
         return values[-1] - values[0]
 
     def compute(rounds: int) -> int:
         elem_counts = dict(Counter(polymer))
-        pair_counts = {polymer[i:(i + 2)]: 1 for i in range(len(polymer) - 1)}
+        pair_counts = dict(Counter([polymer[i:(i + 2)] for i in range(len(polymer) - 1)]))
 
         for i in range(rounds):
-            for pair in list(pair_counts.keys()):
-                new = rules.get(pair, None)
-                if new:
-                    l = pair[0] + new
-                    r = new + pair[1]
+            for pair, pair_count in list(pair_counts.items()):
+                new = rules[pair]
+                l = pair[0] + new
+                r = new + pair[1]
 
-                    pair_counts[pair] -= 1
-                    if pair_counts[pair] == 0:
-                        del pair_counts[pair]
+                pair_counts[pair] -= pair_count
+                if pair_counts[pair] == 0:
+                    del pair_counts[pair]
 
-                    pair_counts[l] = pair_counts.get(l, 0) + 1
-                    pair_counts[r] = pair_counts.get(r, 0) + 1
-                    elem_counts[new] = elem_counts.get(new, 0) + 1
+                pair_counts[l] = pair_counts.get(l, 0) + pair_count
+                pair_counts[r] = pair_counts.get(r, 0) + pair_count
+                elem_counts[new] = elem_counts.get(new, 0) + pair_count
 
         return score(elem_counts)
 
-    print(f'Test: {compute(6)}')
     print(f'Part 1: {compute(10)}')
     print(f'Part 2: {compute(40)}')
