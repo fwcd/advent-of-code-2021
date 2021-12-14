ba10763ae63e2f666859e02886541847f2607f1a
fwcd <fwcdmail@gmail.com>
2021-12-14 06:18:40 +0100

    Complete day 14 part 1 in Python

---
+++ day14/src/day14.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+def apply(s: str, rules: list) -> str:
+    for i in reversed(range(len(s) - 1)):
+        for [lhs, rhs] in rules:
+            if s[i:(i + 2)] == lhs:
+                s = s[:i] + s[i] + rhs + s[(i + 1):]
+                break
+    return s
+
+with open('resources/input.txt', 'r') as f:
+    lines = [l.strip() for l in f.readlines()]
+    polymer = lines[0]
+    rules = [l.split(' -> ') for l in lines[2:]]
+
+    for i in range(10):
+        polymer = apply(polymer, rules)
+    
+    counts = Counter(polymer).most_common()
+    part1 = counts[0][1] - counts[-1][1]
+    print(f'Part 1: {part1}')
