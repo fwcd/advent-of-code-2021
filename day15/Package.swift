e9deb497bb7d5576daf36d0015c43c62b1bd7947
fwcd <fwcdmail@gmail.com>
2021-12-15 19:13:58 +0100

    Compute day 15 part 1 for demo with Dijkstra

--- day15/Package.swift
+++ day15/Package.swift
@@ -12,12 +12,17 @@ let package = Package(
             targets: ["Day15"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections.git", .branch("main")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Day15",
-            dependencies: []
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections"),
+            ]
         ),
     ]
 )
--- day15/Sources/Day15/main.swift
+++ day15/Sources/Day15/main.swift
@@ -1,7 +1,62 @@
+import Collections
 import Foundation
+
+struct Point: Hashable {
+    let y: Int
+    let x: Int
+
+    func isInBounds(of grid: [[Int]]) -> Bool {
+        y >= 0 && y < grid.count && x >= 0 && x < grid[y].count
+    }
+
+    func neighbors(on grid: [[Int]]) -> [Point] {
+        [
+            Point(y: y - 1, x: x),
+            Point(y: y + 1, x: x),
+            Point(y: y, x: x - 1),
+            Point(y: y, x: x + 1),
+        ].filter { $0.isInBounds(of: grid) }
+    }
+}
+
+struct WeightedPoint: Comparable {
+    let point: Point
+    let weight: Int
+
+    static func <(lhs: WeightedPoint, rhs: WeightedPoint) -> Bool {
+        lhs.weight < rhs.weight
+    }
+
+    static func ==(lhs: WeightedPoint, rhs: WeightedPoint) -> Bool {
+        lhs.weight == rhs.weight
+    }
+}
+
+func dijkstra(
+    on grid: [[Int]],
+    from start: Point = Point(y: 0, x: 0),
+    to dest: Point = Point(y: grid.count - 1, x: grid[0].count - 1)
+) -> Int {
+    var heap = Heap<WeightedPoint>()
+    var visited = Set<Point>()
+
+    heap.insert(WeightedPoint(point: start, weight: 0))
+
+    while let next = heap.popMin() {
+        if next.point == dest {
+            return next.weight
+        }
+        visited.insert(next.point)
+        for neighbor in next.point.neighbors(on: grid) where !visited.contains(neighbor) {
+            heap.insert(WeightedPoint(point: neighbor, weight: next.weight + grid[neighbor.y][neighbor.x]))
+        }
+    }
+
+    fatalError("No path found!")
+}
 
 let input = try! String(contentsOfFile: "Resources/demo.txt")
 let grid: [[Int]] = input.split(separator: "\n")
     .map { $0.compactMap { Int(String($0)) } }
 
-
+print("Part 1: \(dijkstra(on: grid))")
