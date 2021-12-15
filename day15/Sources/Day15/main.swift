bd10ae18c886b3dcf3b4b36689b7bbcf9d2a8531
fwcd <fwcdmail@gmail.com>
2021-12-15 18:52:13 +0100

    Read day 15 input

--- day15/Sources/Day15/main.swift
+++ day15/Sources/Day15/main.swift
@@ -1,1 +1,7 @@
-print("Hello")
+import Foundation
+
+let input = try! String(contentsOfFile: "Resources/demo.txt")
+let grid: [[Int]] = input.split(separator: "\n")
+    .map { $0.compactMap { Int(String($0)) } }
+
+
