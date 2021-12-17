c08841c34641f9672d5f82b128c1f99037bc9ce4
fwcd <fwcdmail@gmail.com>
2021-12-17 06:05:54 +0100

    Parse input

--- day17/bin/main/dev/fwcd/adventofcode2021/Day17.kt
+++ day17/bin/main/dev/fwcd/adventofcode2021/Day17.kt
@@ -1,6 +1,10 @@
 package dev.fwcd.adventofcode2021
 
+data class Vec2(val x: Int, val y: Int)
+
 fun main() {
     val input = object {}.javaClass.getResource("/demo.txt").readText()
-    println(input)
+    val pattern = """target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)""".toRegex()
+    val (x1, x2, y1, y2) = pattern.find(input)!!.groupValues.drop(1).map { it.toInt() }
+    println("Got $x1, $x2, $y1, $y2")
 }
