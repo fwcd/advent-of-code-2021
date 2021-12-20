c6f883a82fe7a7623e1c7dbbd689198db0fa1190
fwcd <fwcdmail@gmail.com>
2021-12-20 14:41:05 +0100

    Abstract day 20 solution

--- day20/src/day20.rb
+++ day20/src/day20.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 require 'set'
 
-algo, input = File.read('resources/input.txt').split("\n\n")
-input = input.split
-
 def neighborhood(point)
   y, x = point
   (-1..1).flat_map do |dy|
@@ -29,26 +26,31 @@ def extreme_point(points)
 end
 
 def stringify(steps, points, algo)
-  min_x, min_y = extreme_point(points) { |vs| vs.min }
-  max_x, max_y = extreme_point(points) { |vs| vs.max }
-  ((min_x - steps)..(max_x + steps)).map do |y|
-    ((min_y - steps)..(max_y + steps)).map do |x|
+  min_y, min_x = extreme_point(points) { |vs| vs.min }
+  max_y, max_x = extreme_point(points) { |vs| vs.max }
+  ((min_y - steps)..(max_y + steps)).map do |y|
+    ((min_x - steps)..(max_x + steps)).map do |x|
       point_at?([y, x], steps, points, algo) ? '#' : '.'
     end.join
   end.join("\n")
 end
 
-points = (0...input.length).flat_map do |y|
-  (0...input[y].length).filter_map do |x|
+def compute_count(steps, points, height, width, algo)
+  ((-steps)...(height + steps)).map do |y|
+    ((-steps)...(width + steps)).count do |x|
+      point_at?([y, x], steps, points, algo)
+    end
+  end.sum
+end
+
+algo, input = File.read('resources/input.txt').split("\n\n")
+input = input.split
+height = input.length
+width = input[0].length
+points = (0...height).flat_map do |y|
+  (0...width).filter_map do |x|
     [y, x] if input[y][x] == '#'
   end
 end.to_set
 
-steps = 2
-part1 = ((-steps)...(input.length + steps)).map do |y|
-  ((-steps)...(input[0].length + steps)).count do |x|
-    point_at?([y, x], steps, points, algo)
-  end
-end.sum
-
-puts "Part 1: #{part1}"
+puts "Part 1: #{compute_count(2, points, height, width, algo)}"
