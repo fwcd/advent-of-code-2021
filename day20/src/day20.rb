cae2afeb481bdb802b50c2b9b13de8b85c7d6cf6
fwcd <fwcdmail@gmail.com>
2021-12-20 08:17:22 +0100

    Compute set from input

--- day20/src/day20.rb
+++ day20/src/day20.rb
@@ -1,4 +1,13 @@
 #!/usr/bin/env ruby
+require 'set'
+
 img_enhancement_algo, input = File.read('resources/demo.txt').split("\n\n")
 input = input.split
-puts input
+
+points = (0...input.length).flat_map do |y|
+  (0...input[y].length).filter_map do |x|
+    [y, x] if input[y][x] == '#'
+  end
+end.to_set
+
+puts points
