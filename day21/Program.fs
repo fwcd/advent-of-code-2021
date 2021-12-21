3645cd9127136540721d943041283b1e50cc6d1d
fwcd <fwcdmail@gmail.com>
2021-12-21 13:30:39 +0100

    Add stepping in day 21 program

--- day21/Program.fs
+++ day21/Program.fs
@@ -9,25 +9,34 @@ type State =
     p2: Player
     die: int }
 
+// TODO: Consider case where first player wins
 let chop x = ((x - 1) % 10) + 1
 let step s =
   let pos1' = chop (s.p1.pos + 3 * s.die + 3)
   let pos2' = chop (s.p2.pos + 3 * s.die + 12)
-  let die'  = chop (s.die + 6)
-  { p1  = { pos = pos1'
-            score = s.p1.score + pos1' }
-    p2  = { pos = pos2'
-            score = s.p2.score + pos2' }
+  let die'  = s.die + 6
+  { p1 = { pos = pos1'
+           score = s.p1.score + pos1' }
+    p2 = { pos = pos2'
+           score = s.p2.score + pos2' }
     die = die' }
 
-let rec iterate n f x =
-  if n = 0 then x
-  elif n > 0 then f (iterate (n - 1) f x)
-  else failwith "Invalid n"
+let rec iterateUntil p f x =
+  if p x then x
+  else iterateUntil p f (f x)
 
 let input = File.ReadAllText("resources/demo.txt").Split("\n")
               |> Seq.filter (fun l -> String.length l > 0)
               |> Seq.map (fun l -> (l.Split(":")[1]).Trim() |> int)
               |> Seq.toList
 
-printfn "%d" (iterate 10 (fun x -> x + 1) input[0])
+let initialState =
+  { p1 = { pos = input[0]
+           score = 0 }
+    p2 = { pos = input[1]
+           score = 0 }
+    die = 1
+  }
+let finalState = iterateUntil (fun s -> (max s.p1.score s.p2.score) >= 1000) step initialState
+
+printfn "Part 1: %d" (finalState.die)
