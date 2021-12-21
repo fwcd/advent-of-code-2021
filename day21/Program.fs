396cf3c774270ea596ec9314864450b2bab7c7cd
fwcd <fwcdmail@gmail.com>
2021-12-21 13:38:04 +0100

    Compute day 21 demo score

--- day21/Program.fs
+++ day21/Program.fs
@@ -7,23 +7,39 @@ type Player =
 type State =
   { p1: Player
     p2: Player
+    turn: bool
     die: int }
-
-// TODO: Consider case where first player wins
-let chop x = ((x - 1) % 10) + 1
-let step s =
-  let pos1' = chop (s.p1.pos + 3 * s.die + 3)
-  let pos2' = chop (s.p2.pos + 3 * s.die + 12)
-  let die'  = s.die + 6
-  { p1 = { pos = pos1'
-           score = s.p1.score + pos1' }
-    p2 = { pos = pos2'
-           score = s.p2.score + pos2' }
-    die = die' }
 
 let rec iterateUntil p f x =
   if p x then x
   else iterateUntil p f (f x)
+
+let chop x = ((x - 1) % 10) + 1
+let nextPos pos die = chop (pos + 3 * die + 3)
+let nextDie die = die + 3
+
+let step s =
+  let turn' = not s.turn
+  if s.turn then
+    let pos' = nextPos s.p1.pos s.die
+    { p1 = { pos = pos'
+             score = s.p1.score + pos' }
+      p2 = s.p2
+      die = nextDie s.die
+      turn = turn' }
+  else
+    let pos' = nextPos s.p2.pos s.die
+    { p1 = s.p1
+      p2 = { pos = pos'
+             score = s.p2.score + pos' }
+      die = nextDie s.die
+      turn = turn' }
+
+let threshold = 1000
+let play = iterateUntil (fun s -> (max s.p1.score s.p2.score) >= threshold) step
+let loser s = if s.p1.score >= 1000 then s.p2 else s.p1
+
+// Main program
 
 let input = File.ReadAllText("resources/demo.txt").Split("\n")
               |> Seq.filter (fun l -> String.length l > 0)
@@ -36,7 +52,9 @@ let initialState =
     p2 = { pos = input[1]
            score = 0 }
     die = 1
+    turn = true
   }
-let finalState = iterateUntil (fun s -> (max s.p1.score s.p2.score) >= 1000) step initialState
+let finalState = play initialState
+let part1 = (finalState.die - 1) * (loser finalState).score
 
-printfn "Part 1: %d" (finalState.die)
+printfn "Part 1: %d" part1
