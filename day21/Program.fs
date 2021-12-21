3c08bed846e5ee595653f7619401d7044d970ea3
fwcd <fwcdmail@gmail.com>
2021-12-21 21:11:18 +0100

    Tweak part 2 algorithm

--- day21/Program.fs
+++ day21/Program.fs
@@ -22,9 +22,6 @@ let rec iterateUntil (p: 'a -> bool) (f: 'a -> 'a) (x: 'a) =
   if p x then x
   else iterateUntil p f (f x)
 
-let changeWithDefault (k: 'k) (f: 'v -> 'v) (x: 'v) (m: Map<'k, 'v>) =
-  Map.add k (f (defaultArg (Map.tryFind k m) x)) m
-
 let chop x = ((x - 1) % 10) + 1
 let nextPos pos die = chop (pos + die)
 let step die s =
@@ -43,28 +40,32 @@ let step die s =
       turn = turn' }
 
 let part1Won p = p.score >= 1000
-let part1Step s = { state = step (3 * s.die + 3) s.state; die = s.die + 3 }
+let part1Step ps = { state = step (3 * ps.die + 3) ps.state; die = ps.die + 3 }
 let part1Play = iterateUntil (fun s -> part1Won s.state.p1 || part1Won s.state.p2) part1Step
 let part1Loser s = if part1Won s.p1 then s.p2 else s.p1
 
 let part2Won p = p.score >= 21
 let part2WonStates pf = Map.fold (fun x s c -> x + (if part2Won (pf s) then c else 0UL)) 0UL
-let part2RollDie sts die =
+let part2RollDie sts sts' die =
   Map.keys sts
-    |> Seq.fold (fun sts' s -> changeWithDefault (step die s) (fun x -> x + (defaultArg (Map.tryFind s sts') 0UL)) 0UL sts') sts
-let part2Step s =
-  let states' = (seq { 0..2 }) |> Seq.fold part2RollDie s.states
-  printf "%A\n" states'
+    |> Seq.fold (fun sts'' s ->
+      let c = Map.find s sts
+      sts''
+        |> Map.change s (fun x -> Some ((defaultArg x 0UL) - c) |> Option.filter (fun x -> x > 0UL))
+        |> Map.change (step die s) (fun x -> Some ((defaultArg x 0UL) + c))) sts'
+let part2Step ps =
+  printfn "%A" ps
+  let states' = (seq { 1..3 }) |> Seq.fold (part2RollDie ps.states) ps.states
   { states = Map.filter (fun s _ -> not (part2Won s.p1 || part2Won s.p2)) states'
-    p1Won = s.p1Won + part2WonStates (fun s -> s.p1) states'
-    p2Won = s.p2Won + part2WonStates (fun s -> s.p2) states'
+    p1Won = ps.p1Won + part2WonStates (fun s -> s.p1) states'
+    p2Won = ps.p2Won + part2WonStates (fun s -> s.p2) states'
   }
 let part2Play = iterateUntil (fun s -> Map.isEmpty s.states) part2Step
 
 // Main program
 
 let input =
-  File.ReadAllText("resources/input.txt").Split("\n")
+  File.ReadAllText("resources/demo.txt").Split("\n")
     |> Seq.filter (fun l -> String.length l > 0)
     |> Seq.map (fun l -> (l.Split(":")[1]).Trim() |> int)
     |> Seq.toList
