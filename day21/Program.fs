49391c966fc5a72d760bdefaffad3fbcaeca6cb6
fwcd <fwcdmail@gmail.com>
2021-12-21 13:07:12 +0100

    Define basic game strucutures and step fn for day 21

--- day21/Program.fs
+++ day21/Program.fs
@@ -1,5 +1,25 @@
 ﻿open System.IO
 
+type Player =
+  { pos: int
+    score: int }
+
+type State =
+  { p1: Player
+    p2: Player
+    die: int }
+
+let chop x = ((x - 1) % 10) + 1
+let step s =
+  let pos1' = chop (s.p1.pos + 3 * s.die + 3)
+  let pos2' = chop (s.p2.pos + 3 * s.die + 12)
+  let die'  = chop (s.die + 6)
+  { p1  = { pos = pos1'
+            score = s.p1.score + pos1' }
+    p2  = { pos = pos2'
+            score = s.p2.score + pos2' }
+    die = die' }
+
 let input = File.ReadAllText("resources/demo.txt").Split("\n")
               |> Seq.filter (fun l -> String.length l > 0)
               |> Seq.map (fun l -> (l.Split(":")[1]).Trim() |> int)
