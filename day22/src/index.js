d072ef97d4652613f5bd6086c1b1b2680354a37c
fwcd <fwcdmail@gmail.com>
2021-12-22 06:17:21 +0100

    Solve day 22 part 1

--- day22/src/index.js
+++ day22/src/index.js
@@ -1,8 +1,28 @@
 import * as fs from 'fs';
 
 async function main() {
-  const input = await fs.promises.readFile('resources/demo.txt', { encoding: 'utf-8' });
-  console.log(input);
+  const input = await fs.promises.readFile('resources/input.txt', { encoding: 'utf-8' });
+  const cubes = input
+    .split('\n')
+    .filter(l => l.length > 0)
+    .map(l => [.../(on|off) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/.exec(l)].slice(1));
+
+  let part1 = 0;
+  for (let x = -50; x <= 50; x++) {
+    for (let y = -50; y <= 50; y++) {
+      for (let z = -50; z <= 50; z++) {
+        let on = false;
+        for (const [state, x1, x2, y1, y2, z1, z2] of cubes) {
+          if (x1 <= x && x <= x2 && y1 <= y && y <= y2 && z1 <= z && z <= z2) {
+            on = state === 'on';
+          }
+        }
+        part1 += on ? 1 : 0;
+      }
+    }
+  }
+
+  console.log(`Part 1: ${part1}`);
 }
 
 main();
