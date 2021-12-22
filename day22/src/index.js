81ba365f0c8fe84ca884b3012728ebe0e387be55
fwcd <fwcdmail@gmail.com>
2021-12-22 06:00:37 +0100

    Set up JS project

--- day22/src/index.js
+++ day22/src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const fs = require('fs');
+
+async function main() {
+  const input = await fs.promises.readFile('resources/demo.txt', { encoding: 'utf-8' });
+  console.log(input);
+}
+
+main();
