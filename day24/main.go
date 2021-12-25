2a2a138a0151619d4bc2d58d55f9007fae06ae0d
fwcd <fwcdmail@gmail.com>
2021-12-25 02:05:31 +0100

    Implement MONAD step function in Go

--- day24/main.go
+++ day24/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func step(w int, z int, q int, a int, b int) int {
+	var x int
+
+	if ((z%26)/q + a) != w {
+		x = 1
+	} else {
+		x = 0
+	}
+
+	return z*(25*x+1) + (w+b)*x
+}
+
 func main() {
 	fmt.Println("Hello world")
 }
