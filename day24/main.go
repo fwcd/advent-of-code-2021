466a0899e93221de5f03d61003da84853771e16a
fwcd <fwcdmail@gmail.com>
2021-12-25 02:24:03 +0100

    Parse day 24 parameters

--- day24/main.go
+++ day24/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func step(w int, z int, q int, a int, b int) int {
 	var x int
@@ -14,6 +25,22 @@ func step(w int, z int, q int, a int, b int) int {
 	return z*(25*x+1) + (w+b)*x
 }
 
+func parseOperand(line string) int {
+	xs := strings.Split(line, " ")
+	x, err := strconv.Atoi(xs[len(xs)-1])
+	check(err)
+	return x
+}
+
 func main() {
-	fmt.Println("Hello world")
+	raw, err := os.ReadFile("resources/input.txt")
+	check(err)
+
+	lines := strings.Split(string(raw), "\n")
+	for i := 0; i < (len(lines)/18)*18; i += 18 {
+		q := parseOperand(lines[i+4])
+		a := parseOperand(lines[i+5])
+		b := parseOperand(lines[i+15])
+		fmt.Printf("q = %d, a = %d, b = %d\n", q, a, b)
+	}
 }
